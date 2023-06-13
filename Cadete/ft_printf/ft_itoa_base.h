d641aa41bd08551950b1c20be3a11388faee0147
Lisias De castro martins <lde-cast@c3r5p1.42sp.org.br>
2023-06-13 18:05:54 -0300

    cadete42

--- Cadete/ft_printf/ft_itoa_base.h
+++
@@ -1,9 +0,0 @@
-#ifndef FT_ITOA_BASE_H
-#define FT_ITOA_BASE_H
-
-extern void ft_putchar_fd(char c, int fd);
-extern int	ft_itoa_base(int nb, unsigned char base, unsigned char upper);
-extern int	ft_itoa_base_unsigned(unsigned nb, unsigned char base, unsigned char upper);
-extern int	ft_itoa_base_long(unsigned long nb, unsigned char base, unsigned char upper);
-
-#endif // FT_ITOA_BASE_H
--- Cadete/ft_printf/ft_printf.h
+++
@@ -1,9 +0,0 @@
-#ifndef FT_PRINTF_H
-#define FT_PRINTF_H
-
-#include <stdarg.h>
-#include "ft_itoa_base.h"
-
-extern int	ft_printf(const char *str, ...);
-
-#endif // FT_PRINTF_H
